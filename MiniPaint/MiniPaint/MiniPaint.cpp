f30d3c53531e53fa7a33613fd37f5e39136bbfcc
Veronika Kosilo <Kosia_nika@mail.ru>
2014-09-29 21:09:43 +0300

    Revert "Lab 1"
    This reverts commit 8b04d337cf255f664a7ae454a8aa5dca05d7da4d.
    
    Conflicts:
    	MiniPaint/Debug/MiniPaint.exe
    	MiniPaint/Debug/MiniPaint.ilk
    	MiniPaint/Debug/MiniPaint.pdb
    	MiniPaint/MiniPaint.sdf
    	MiniPaint/MiniPaint.v11.suo
    	MiniPaint/MiniPaint/Debug/CL.read.1.tlog
    	MiniPaint/MiniPaint/Debug/MiniPaint.log
    	MiniPaint/MiniPaint/Debug/MiniPaint.obj
    	MiniPaint/MiniPaint/Debug/MiniPaint.res
    	MiniPaint/MiniPaint/Debug/vc110.idb
    	MiniPaint/MiniPaint/Debug/vc110.pdb
    	MiniPaint/MiniPaint/MiniPaint.aps
    	MiniPaint/MiniPaint/MiniPaint.cpp
    	MiniPaint/MiniPaint/MiniPaint.rc
    	MiniPaint/MiniPaint/resource.h

--- MiniPaint/MiniPaint/MiniPaint.cpp
+++ MiniPaint/MiniPaint/MiniPaint.cpp
@@ -3,20 +3,23 @@
 
 #include "stdafx.h"
 #include "MiniPaint.h"
+<<<<<<< HEAD
 #include <commdlg.h>
 #include "Resource.h"
+=======
+>>>>>>> parent of 8b04d33... Lab 1
 
 #define MAX_LOADSTRING 100
 // Идентификатор ресурса шаблона меню
 #define IDM_MYMENURESOURCE 3
-//#define PENCIL 8
-//#define CREATE 9
-//#define OPEN 10
-//#define BLACK 13
-//#define LINE 14
-//#define RECTANGLE 15
-//#define ELLIPSE 16
-//#define CIRCLE 17
+#define PENCIL 8
+#define CREATE 9
+#define OPEN 10
+#define BLACK 13
+#define LINE 14
+#define RECTANGLE 15
+#define ELLIPSE 16
+#define CIRCLE 17
 
 
 
@@ -25,13 +28,16 @@
 HINSTANCE hInst;								// текущий экземпляр
 TCHAR szTitle[MAX_LOADSTRING];					// Текст строки заголовка
 TCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+HMENU Init_MENU();
 HWND hWnd;
 HWND hDrawWnd;
-HWND hText;
+HPEN MyPen;
+RECT rect;
+HBRUSH MyBrush;
 HDC hDC;
 HDC hMemDC;
-HDC hFile;
 HBITMAP hBit;
+<<<<<<< HEAD
 RECT rect;
 HPEN myPen;
 HBRUSH myBrush;
@@ -48,10 +54,14 @@ bool flag = false;
 bool zoomOrPan = false;
 bool saveFirstTime = true;
 TCHAR File[MAX_PATH];
+=======
+int operation_number = 0, color_number = BLACK, width = 1;
+int x, y;
+int r=0, g=0, b=0;
+>>>>>>> parent of 8b04d33... Lab 1
 
 // Отправить объявления функций, включенных в этот модуль кода:
 ATOM				MyRegisterClass(HINSTANCE hInstance);
-ATOM				MyRegisterDrawClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
@@ -74,32 +84,15 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
 	LoadString(hInstance, IDC_MINIPAINT, szWindowClass, MAX_LOADSTRING);
 	MyRegisterClass(hInstance);
+	RegisterChildWnd(hInstance);
 
 	// Выполнить инициализацию приложения:
 	if (!InitInstance (hInstance, nCmdShow))
 	{
 		return FALSE;
 	}
-	ACCEL accKey[5];
-	accKey[0].cmd = ID_CREATE;
-	accKey[0].fVirt = FVIRTKEY | FCONTROL;
-	accKey[0].key = 0x4E;
-	accKey[1].cmd = IDM_EXIT;
-	accKey[1].fVirt = FVIRTKEY | FCONTROL;
-	accKey[1].key = VK_F4;
-	accKey[2].cmd = ID_OPEN;
-	accKey[2].fVirt = FVIRTKEY | FCONTROL;
-	accKey[2].key = 0x4F;
-	accKey[3].cmd = ID_PRINT;
-	accKey[3].fVirt = FVIRTKEY | FCONTROL;
-	accKey[3].key = 0x50;
-	accKey[4].cmd = ID_SAVE;
-	accKey[4].fVirt = FVIRTKEY | FCONTROL;
-	accKey[4].key = 0x53;
-	hAccelTable = CreateAcceleratorTable((LPACCEL)accKey, 5);
-	MyRegisterDrawClass(hInst);
 
-	//hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MINIPAINT));
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MINIPAINT));
 
 	// Цикл основного сообщения:
 	while (GetMessage(&msg, NULL, 0, 0))
@@ -127,7 +120,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 
 	wcex.cbSize = sizeof(WNDCLASSEX);
 
-	wcex.style			= CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
 	wcex.lpfnWndProc	= WndProc;
 	wcex.cbClsExtra		= 0;
 	wcex.cbWndExtra		= 0;
@@ -135,34 +128,33 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDC_icon));
 	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
 	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_MINIPAINT);
+	wcex.lpszMenuName	= NULL/*MAKEINTRESOURCE(IDC_MINIPAINT)*/;
 	wcex.lpszClassName	= szWindowClass;
 	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
 	return RegisterClassEx(&wcex);
 }
 
-ATOM MyRegisterDrawClass(HINSTANCE hInstance)
+ATOM RegisterChildWnd(HINSTANCE hInstance)
 {
 	WNDCLASSEX wcex;
 
 	wcex.cbSize = sizeof(WNDCLASSEX);
 
-	wcex.style			= CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+	wcex.style			= CS_HREDRAW | CS_VREDRAW ;
 	wcex.lpfnWndProc	= ChildProc;
 	wcex.cbClsExtra		= 0;
 	wcex.cbWndExtra		= 0;
 	wcex.hInstance		= hInstance;
 	wcex.hIcon			= NULL;
-	wcex.hCursor		= LoadCursor(NULL, IDC_CROSS);
-	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(6);
 	wcex.lpszMenuName	= NULL;
-	wcex.lpszClassName	= L"ChildWnd";
+	wcex.lpszClassName	= L"ChildWindow";
 	wcex.hIconSm		= NULL;
 
 	return RegisterClassEx(&wcex);
 }
-
 
 //
 //   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
@@ -181,7 +173,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    hInst = hInstance; // Сохранить дескриптор экземпляра в глобальной переменной
 
    hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, Init_MENU(), hInstance, NULL);
 
    if (!hWnd)
    {
@@ -208,20 +200,24 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	int wmId, wmEvent;
 	PAINTSTRUCT ps;
-	HENHMETAFILE h;
-	TCHAR szFile[MAX_PATH];
+	HDC hDC;
 
 	switch (message)
 	{
+<<<<<<< HEAD
 	case WM_CREATE:
 		logbrush.lbStyle = BS_HOLLOW;
 		break;
+=======
+		//
+>>>>>>> parent of 8b04d33... Lab 1
 	case WM_COMMAND:
 		wmId    = LOWORD(wParam);
 		wmEvent = HIWORD(wParam);
 		// Разобрать выбор в меню:
 		switch (wmId)
 		{
+<<<<<<< HEAD
 		case IDM_ABOUT:
 			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
 			break;
@@ -445,17 +441,27 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			hDrawWnd = CreateWindow(L"ChildWnd", NULL, WS_CHILD | WS_BORDER,
 				1, 1, 800, 400, hWnd, NULL, hInst, NULL);
 
+=======
+		case CREATE:
+			//hDrawWnd = CreateBitmap(600,400,1,100,NULL)
+			hDrawWnd = CreateWindow(L"ChildWindow", NULL, WS_CHILD | WS_BORDER, 1, 1, 600, 400, hWnd, NULL, hInst, NULL);
+>>>>>>> parent of 8b04d33... Lab 1
 			hDC = GetDC(hDrawWnd);
-			hMemDC = CreateCompatibleDC(hDC);
+			hMemDC = CreateCompatibleDC(hDC);//
 			GetClientRect(hDrawWnd, &rect);
 			hBit = CreateCompatibleBitmap(hDC, rect.right, rect.bottom);
+<<<<<<< HEAD
 			myBrush = CreateSolidBrush(RGB(255,255,255));
+=======
+			MyBrush = CreateSolidBrush(RGB(255, 255, 255));
+>>>>>>> parent of 8b04d33... Lab 1
 			SelectObject(hMemDC, hBit);
-			FillRect(hMemDC, &rect, myBrush);
-			DeleteObject(myBrush);
+			FillRect(hMemDC, &rect, MyBrush);
+			DeleteObject(MyBrush);
 			ReleaseDC(hDrawWnd, hDC);
 			ShowWindow(hDrawWnd, SW_SHOW);
 			UpdateWindow(hDrawWnd);
+<<<<<<< HEAD
 			m = 1;
 			xZ = 0;
 			yZ = 0;
@@ -569,90 +575,30 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			}
 			flag = false;
 			penWidth = 1;
+=======
+>>>>>>> parent of 8b04d33... Lab 1
 			break;
-		case ID_TWO:
-			if (hText != NULL)
-			{
-				SetFocus(NULL);
-				DestroyWindow(hText);
-			}
-			flag = false;
-			penWidth = 2;
+		case PENCIL:
+			operation_number = PENCIL;
 			break;
-		case ID_THREE:
-			if (hText != NULL)
-			{
-				SetFocus(NULL);
-				DestroyWindow(hText);
-			}
-			flag = false;
-			penWidth = 3;
+		case LINE:
+			operation_number = LINE;
 			break;
-		case ID_FOUR:
-			if (hText != NULL)
-			{
-				SetFocus(NULL);
-				DestroyWindow(hText);
-			}
-			flag = false;
-			penWidth = 4;
+		case RECTANGLE:
+			operation_number = RECTANGLE;
 			break;
-		case ID_FIVE:
-			if (hText != NULL)
-			{
-				SetFocus(NULL);
-				DestroyWindow(hText);
-			}
-			flag = false;
-			penWidth = 5;
+		case CIRCLE:
+			operation_number = CIRCLE;
 			break;
-		case ID_SIX:
-			if (hText != NULL)
-			{
-				SetFocus(NULL);
-				DestroyWindow(hText);
-			}
-			flag = false;
-			penWidth = 6;
+		case ELLIPSE:
+			operation_number = ELLIPSE;
 			break;
-		case ID_SEVEN:
-			if (hText != NULL)
-			{
-				SetFocus(NULL);
-				DestroyWindow(hText);
-			}
-			flag = false;
-			penWidth = 7;
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
 			break;
-		case ID_EIGHT:
-			if (hText != NULL)
-			{
-				SetFocus(NULL);
-				DestroyWindow(hText);
-			}
-			flag = false;
-			penWidth = 8;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
 			break;
-		case ID_NINE:
-			if (hText != NULL)
-			{
-				SetFocus(NULL);
-				DestroyWindow(hText);
-			}
-			flag = false;
-			penWidth = 9;
-			break;
-		case ID_TEN:
-			if (hText != NULL)
-			{
-				SetFocus(NULL);
-				DestroyWindow(hText);
-			}
-			flag = false;
-			penWidth = 10;
-			break;
-
-
 		default:
 			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
@@ -660,8 +606,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_PAINT:
 		hDC = BeginPaint(hWnd, &ps);
 		// TODO: добавьте любой код отрисовки...
+			ValidateRect(hWnd, NULL);
 		EndPaint(hWnd, &ps);
 		break;
+<<<<<<< HEAD
 
 	case WM_LBUTTONDOWN:
 		if (zoomOrPan)
@@ -955,6 +903,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 		break;
 
+=======
+>>>>>>> parent of 8b04d33... Lab 1
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
@@ -983,116 +933,87 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 	}
 	return (INT_PTR)FALSE;
 }
+HMENU Init_MENU()
+{
+	HMENU hMenu = CreateMenu();
+	HMENU hPopUp = CreatePopupMenu();
+	HMENU hCPopUp = CreatePopupMenu();
+	HMENU hWid = CreatePopupMenu();
 
+	AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)hPopUp, L"File");
+	AppendMenu(hMenu, MF_STRING,PENCIL,L"Pencil");
+	AppendMenu(hMenu, MF_STRING,LINE,L"Line");
+	AppendMenu(hMenu, MF_STRING,ELLIPSE,L"Ellipse");
+	AppendMenu(hMenu, MF_STRING,RECTANGLE,L"Rectangle");
+	AppendMenu(hMenu, MF_STRING,CIRCLE,L"Circle");
 
-LRESULT CALLBACK ChildProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+	AppendMenu(hPopUp, MF_STRING, CREATE, L"Create");
+	AppendMenu(hPopUp, MF_SEPARATOR, NULL, L"");
+	AppendMenu(hPopUp, MF_STRING, OPEN, L"Open...");
+	//AppendMenu(hPopUp, MF_STRING, SAVE, L"Save");
+	//AppendMenu(hPopUp, MF_STRING, SAVE_AS, L"Save As...");
+	AppendMenu(hPopUp, MF_SEPARATOR, NULL, L"");
+	AppendMenu(hPopUp, MF_STRING, IDM_EXIT, L"Exit");
+	AppendMenu(hPopUp, MF_SEPARATOR, NULL, L"");
+	AppendMenu(hPopUp, MF_STRING, IDM_ABOUT, L"About...");
+	return hMenu;
+}
+
+LRESULT CALLBACK ChildProc(HWND hWnd, UINT Message, WPARAM wParam, LPARAM lParam)
 {
 	int wmId, wmEvent;
 	PAINTSTRUCT ps;
+	int x2, y2;
 
-	switch (message)
+	switch (Message)
 	{
 	case WM_COMMAND:
 		wmId = LOWORD(wParam);
 		wmEvent = HIWORD(wParam);
-		// Разобрать выбор в меню:
 		switch (wmId)
 		{
-		default:
-			return DefWindowProc(hWnd, message, wParam, lParam);
-		}
-		break;
-	case WM_PAINT:
-		hDC = BeginPaint(hWnd, &ps);
 
-		hDC = GetDC(hDrawWnd);
-		GetClientRect(hDrawWnd, &rect);
-		myBrush = CreateSolidBrush(RGB(255, 255, 255));
-		SelectObject(hDC, myBrush);
-		FillRect(hDC, &rect, myBrush);
-		StretchBlt(hDC, 0 + xZ, 0 + yZ, rect.right, rect.bottom, hMemDC, 0 + xZ, 0 + yZ, (int)rect.right*m, (int)rect.bottom*m, SRCCOPY);
-		ReleaseDC(hDrawWnd, hDC);
-		// TODO: добавьте любой код отрисовки...
-		EndPaint(hWnd, &ps);
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+
+		default:
+			return DefWindowProc(hWnd, Message, wParam, lParam);
+		}
 		break;
 
 	case WM_LBUTTONDOWN:
-		if (zoomOrPan)
-			break;
-		if (!flag)
-		{
-			x0 = LOWORD(lParam);
-			y0 = HIWORD(lParam);
-		}
-		if ((operation == ID_TEXT) && (hText != NULL))
-		{
-			SetFocus(NULL);
-			DestroyWindow(hText);
-		}
-		if ((operation == ID_POLYLINE) || (operation == ID_POLYGON))
-		{
-			if (flag)
-			{
-				hDC = GetDC(hDrawWnd);
-				GetClientRect(hDrawWnd, &rect);
-				BitBlt(hMemDC, 0, 0, rect.right, rect.bottom, hDC, 0, 0, SRCCOPY);
-			}
-			else
-				flag = true;
-		}
-
-		x1 = LOWORD(lParam);
-		y1 = HIWORD(lParam);
-
-
+		//SetCapture(hWnd);
+		x = LOWORD(lParam);
+		y = HIWORD(lParam);
 		break;
 
 	case WM_MOUSEMOVE:
-		if (zoomOrPan)
-			break;
-		if (hDrawWnd == NULL)
-			break;
 		if (MK_LBUTTON == wParam)
 		{
-			switch (operation)
+			switch (operation_number)
 			{
-			case ID_PENCIL:
+			case PENCIL:
 				x2 = LOWORD(lParam);
 				y2 = HIWORD(lParam);
-				myPen = CreatePen(PS_SOLID, penWidth, rgb);
+				MyPen = CreatePen(PS_SOLID, width, RGB(r, g, b));
 				hDC = GetDC(hDrawWnd);
 				GetClientRect(hDrawWnd, &rect);
-				SelectObject(hMemDC, myPen);
-				MoveToEx(hMemDC, x1, y1, NULL);
+				BitBlt(hDC,rect.left, rect.top, rect.right, rect.bottom, hMemDC,rect.left, rect.top, SRCCOPY);
+				//SelectClipRgn(hDC,rect);
+				MoveToEx(hMemDC, x, y, NULL);
 				LineTo(hMemDC, x2, y2);
-
-				BitBlt(hDC, 0, 0, rect.right, rect.bottom, hMemDC, 0, 0, SRCCOPY);
-				ReleaseDC(hDrawWnd, hDC);
-				DeleteObject(myPen);
-
-
-				x1 = x2;
-				y1 = y2;
-				break;
-
-			case ID_LINE:
-				x2 = LOWORD(lParam);
-				y2 = HIWORD(lParam);
-
-				myPen = CreatePen(PS_SOLID, penWidth, rgb);
-				hDC = GetDC(hDrawWnd);
-				GetClientRect(hDrawWnd, &rect);
-				BitBlt(hDC, 0, 0, rect.right, rect.bottom, hMemDC, 0, 0, SRCCOPY);
-				SelectObject(hDC, myPen);
-				MoveToEx(hDC, x1, y1, NULL);
+				MoveToEx(hDC, x, y, NULL);
 				LineTo(hDC, x2, y2);
 				ReleaseDC(hDrawWnd, hDC);
-				DeleteObject(myPen);
+				DeleteObject(MyPen);
+				x = x2;
+				y = y2;
 				break;
-
-			case ID_RECT:
-				x2 = LOWORD(lParam);
+			case LINE:
+				/*x2 = LOWORD(lParam);
 				y2 = HIWORD(lParam);
+<<<<<<< HEAD
 				myPen = CreatePen(PS_SOLID, penWidth, rgb);
 				logbrush.lbColor = fillRGB;
 				//logbrush.lbStyle = BS_HOLLOW;
@@ -1107,10 +1028,17 @@ LRESULT CALLBACK ChildProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam
 				DeleteObject(myPen);
 				DeleteObject(myBrush);
 				break;
+=======
+				hDC = GetDC(hDrawWnd);
+				BitBlt(hDC,rect.left, rect.top, rect.right, rect.bottom, hMemDC,rect.left, rect.top, SRCCOPY);
+				MoveToEx(hDC, x, y, NULL);
+				LineTo(hDC, x2, y2);
+				ReleaseDC(hDrawWnd, hDC);*/
+>>>>>>> parent of 8b04d33... Lab 1
 
-			case ID_ELLIPSE:
 				x2 = LOWORD(lParam);
 				y2 = HIWORD(lParam);
+<<<<<<< HEAD
 				myPen = CreatePen(PS_SOLID, penWidth, rgb);
 				logbrush.lbColor = fillRGB;
 				//logbrush.lbStyle = BS_HOLLOW;
@@ -1143,108 +1071,55 @@ LRESULT CALLBACK ChildProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam
 				BitBlt(hDC, 0, 0, rect.right, rect.bottom, hMemDC, 0, 0, SRCCOPY);
 				SelectObject(hDC, myPen);
 				MoveToEx(hDC, x1, y1, NULL);
-				LineTo(hDC, x2, y2);
-				ReleaseDC(hDrawWnd, hDC);
-				DeleteObject(myPen);
-				break;
-
-			case ID_POLYGON:
-				if (flag == false)
-					break;
-				x2 = LOWORD(lParam);
-				y2 = HIWORD(lParam);
-
-				myPen = CreatePen(PS_SOLID, penWidth, rgb);
+=======
+				MyPen = CreatePen(PS_SOLID, width, RGB(r, g, b));
 				hDC = GetDC(hDrawWnd);
 				GetClientRect(hDrawWnd, &rect);
-				BitBlt(hDC, 0, 0, rect.right, rect.bottom, hMemDC, 0, 0, SRCCOPY);
-				SelectObject(hDC, myPen);
-				MoveToEx(hDC, x1, y1, NULL);
+				BitBlt(hDC, 0, 0, rect.right - rect.left, rect.bottom - rect.top, hMemDC, 0, 0, SRCCOPY);
+				MoveToEx(hDC, x, y, NULL);
+>>>>>>> parent of 8b04d33... Lab 1
 				LineTo(hDC, x2, y2);
 				ReleaseDC(hDrawWnd, hDC);
-				DeleteObject(myPen);
+				DeleteObject(MyPen);
 				break;
+			case CIRCLE:
 
+				break;
+			case RECTANGLE:
+				x2 = LOWORD(lParam);
+				y2 = HIWORD(lParam);
+				MyPen = CreatePen(PS_SOLID, width, RGB(r, g, b));
+				hDC = GetDC(hDrawWnd);
+				GetClientRect(hDrawWnd, &rect);
+				BitBlt(hDC, 0, 0, rect.right - rect.left, rect.bottom - rect.top, hMemDC, 0, 0, SRCCOPY);
+				Rectangle(hDC, x, y, x2, y2);
+				ReleaseDC(hDrawWnd, hDC);
+				DeleteObject(MyPen);
+				break;
+			default:
+				break;
 			}
 		}
-
-
-		break;
-
-	case WM_RBUTTONUP:
-		if (zoomOrPan)
-		{
-			hDC = GetDC(hDrawWnd);
-			GetClientRect(hDrawWnd, &rect);
-			BitBlt(hDC, 0, 0, rect.right, rect.bottom, hMemDC, 0, 0, SRCCOPY);
-			ReleaseDC(hDrawWnd, hDC);
-			xZ = 0;
-			yZ = 0;
-			m = 1;
-			zoomOrPan = false;
-		}
-		if ((operation == ID_POLYGON) && (flag))
-		{
-			myPen = CreatePen(PS_SOLID, penWidth, rgb);
-			hDC = GetDC(hDrawWnd);
-			GetClientRect(hDrawWnd, &rect);
-			BitBlt(hDC, 0, 0, rect.right, rect.bottom, hMemDC, 0, 0, SRCCOPY);
-			SelectObject(hDC, myPen);
-			MoveToEx(hDC, x1, y1, NULL);
-			LineTo(hDC, x0, y0);
-			ReleaseDC(hDrawWnd, hDC);
-			DeleteObject(myPen);
-		}
-		if (operation == ID_TEXT)
-		{
-			if (hText == NULL)
-				break;
-			TCHAR s[255] = L"";
-			RECT r;
-			GetClientRect(hText, &r);
-			GetWindowText(hText, s, 255);
-			hDC = GetDC(hDrawWnd);
-			SetFocus(NULL);
-			DestroyWindow(hText);
-			hText = NULL;
-			r.left = x1;
-			r.top = y1;
-			r.bottom = r.top + 20;
-			r.right = r.left + 200;
-			SetBkMode(hMemDC, TRANSPARENT);
-			DrawText(hMemDC, s, 255, &r, DT_LEFT | DT_TOP);
-			BitBlt(hDC, 0, 0, rect.right, rect.bottom, hMemDC, 0, 0, SRCCOPY);
-			ReleaseDC(hDrawWnd, hDC);
-		}
-		flag = false;
-		hDC = GetDC(hDrawWnd);
-		GetClientRect(hDrawWnd, &rect);
-		BitBlt(hMemDC, 0, 0, rect.right, rect.bottom, hDC, 0, 0, SRCCOPY);
-		break;
-
-
 	case WM_LBUTTONUP:
-		if (zoomOrPan)
-			break;
-		if (operation == ID_TEXT)
-		{
-
-			hText = CreateWindow(L"Edit", NULL, WS_CHILD | WS_BORDER | WS_VISIBLE | ES_LEFT , x1, y1, 200, 20, hDrawWnd, NULL, hInst, NULL);
-			SetFocus(hText);
-		}
-		else
-		{
+			//ClipCursor(NULL);
 			hDC = GetDC(hDrawWnd);
-			GetClientRect(hDrawWnd, &rect);
-			BitBlt(hMemDC, 0, 0, rect.right, rect.bottom, hDC, 0, 0, SRCCOPY);
-		}
-		break;
+			BitBlt(hMemDC, 0, 0, rect.right - rect.left, rect.bottom - rect.top, hDC, 0, 0, SRCCOPY);
+			ReleaseDC(hDrawWnd, hDC);
+			ValidateRect(hWnd, NULL);
+			//ClipCursor(NULL);
+			break;
 
+	case WM_PAINT:
+		hDC = BeginPaint(hWnd, &ps);
+			ValidateRect(hWnd, NULL);	// TODO: добавьте любой код отрисовки...
+		EndPaint(hWnd, &ps);
+		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
 	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
+		return DefWindowProc(hWnd, Message, wParam, lParam);
 	}
+
 	return 0;
 }
